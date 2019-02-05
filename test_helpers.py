283526a44774751a34867865eed4005a358956cc
Oliver Nagy <olitheolix@gmail.com>
2019-02-05 13:18:55 +1100

    TLC

--- test_helpers.py
+++ test_helpers.py
@@ -23,5 +23,5 @@ def make_manifest(kind: str, namespace: str, name: str):
     return manifest
 
 
-def mk_deploy(name: str, ns: str="namespace"):
+def mk_deploy(name: str, ns: str = "namespace"):
     return make_manifest("Deployment", ns, name)
