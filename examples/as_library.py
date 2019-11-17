65a3884f3fb2582c1440344f3bea32bec7416b59
Oliver Nagy <olitheolix@gmail.com>
2019-11-17 13:52:17 +0100

    Fixup standalone example

--- examples/as_library.py
+++ examples/as_library.py
@@ -7,32 +7,44 @@ from square.dtypes import GroupBy, Selectors
 
 
 def main():
-    # Define the necessary variables.
-    kubeconfig = pathlib.Path(os.environ["KUBECONFIG"])
-    kubectx = None
-    folder = pathlib.PosixPath('manifests')
+    # ----------------------------------------------------------------------
+    #                                 Setup
+    # ----------------------------------------------------------------------
+    # Kubernetes credentials.
+    kubeconfig, kube_ctx = pathlib.Path(os.environ["KUBECONFIG"]), None
+
+    # Specify the resources to operate on. These ones model the demo setup in
+    # the `../integration-test-cluster`.
     selectors = Selectors(
         kinds=["Deployment", "Service", "Namespace"],
-        labels={("app", "demoapp")},
-        namespaces=["square-tests"],
+        labels={("app", "demoapp-1")},
+        namespaces=["square-tests-1", "square-tests-2"],
     )
+
+    # Where to store the manifests and how to organise them inside that folder.
+    folder = pathlib.PosixPath('manifests')
     groupby = GroupBy(label="app", order=["ns", "label", "kind"])
 
-    # Optional: configures Square's logger.
-    square.square.setup_logging(0)
+    # Optional: Set log level (0 = ERROR, 1 = WARNING, 2 = INFO, 3 = DEBUG).
+    square.square.setup_logging(3)
 
-    # Convenience.
-    common_args = kubeconfig, kubectx, folder, selectors
+    # ----------------------------------------------------------------------
+    #          Import resources, create a plan and then apply it
+    # ----------------------------------------------------------------------
+    # Download the manifests into the `folder` defined earlier.
+    _, err = square.get(kubeconfig, kube_ctx, folder, selectors, groupby)
+    assert not err
 
-    # Download manifests.
-    _, err = square.get(*common_args, groupby)
+    # Compute the plan and show it. It will not show any differences because we
+    # planned against the resources we just downloaded.
+    plan, err = square.plan(kubeconfig, kube_ctx, folder, selectors)
+    assert not err
+    square.show_plan(plan)
 
-    # Compute plan, then print it.
-    plan, err = square.plan(*common_args)
-    square.square.print_deltas(plan)
-
-    # Apply the plan.
-    _, err = square.apply(*common_args, plan, "yes")
+    # Apply the plan - will not actually do anything in this case, because the
+    # plan is empty.
+    _, err = square.apply_plan(kubeconfig, kube_ctx, plan)
+    assert not err
 
 
 if __name__ == "__main__":
