eaa97f185367ec3065c5b603dad3a893b2a9b0dd
Oliver Nagy <olitheolix@gmail.com>
2020-03-07 19:13:12 +0100

    Support K8s version 1.12

--- square/k8s.py
+++ square/k8s.py
@@ -544,6 +544,24 @@ def urlpath(
             "ServiceAccount": f"api/v1/{namespace}/serviceaccounts",
             "StatefulSet": f"apis/apps/v1/{namespace}/statefulsets",
         },
+        "1.12": {
+            "ClusterRole": f"apis/rbac.authorization.k8s.io/v1/clusterroles",
+            "ClusterRoleBinding": f"apis/rbac.authorization.k8s.io/v1/clusterrolebindings",  # NOQA
+            "ConfigMap": f"api/v1/{namespace}/configmaps",
+            "CronJob": f"apis/batch/v1beta1/{namespace}/cronjobs",
+            "DaemonSet": f"apis/apps/v1/{namespace}/daemonsets",
+            "Deployment": f"apis/apps/v1/{namespace}/deployments",
+            "HorizontalPodAutoscaler": f"apis/autoscaling/v1/{namespace}/horizontalpodautoscalers",   # NOQA
+            "Ingress": f"apis/extensions/v1beta1/{namespace}/ingresses",
+            "Namespace": f"api/v1/namespaces",
+            "PersistentVolumeClaim": f"api/v1/{namespace}/persistentvolumeclaims",
+            "Role": f"apis/rbac.authorization.k8s.io/v1/{namespace}/roles",
+            "RoleBinding": f"apis/rbac.authorization.k8s.io/v1/{namespace}/rolebindings",
+            "Secret": f"api/v1/{namespace}/secrets",
+            "Service": f"api/v1/{namespace}/services",
+            "ServiceAccount": f"api/v1/{namespace}/serviceaccounts",
+            "StatefulSet": f"apis/apps/v1/{namespace}/statefulsets",
+        },
         "1.13": {
             "ClusterRole": f"apis/rbac.authorization.k8s.io/v1/clusterroles",
             "ClusterRoleBinding": f"apis/rbac.authorization.k8s.io/v1/clusterrolebindings",  # NOQA
