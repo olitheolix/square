929018357c9443f220ef24bebf9f60aa2bdc7e76
Oliver Nagy <olitheolix@gmail.com>
2020-04-20 07:55:09 +0200

    Add support for Kubernetes v1.15.

--- tests/test_k8s.py
+++ tests/test_k8s.py
@@ -262,7 +262,8 @@ class TestUrlPathBuilder:
         Those variables specify the supported K8s versions and resource types.
 
         """
-        assert SUPPORTED_VERSIONS == ("1.9", "1.10", "1.11", "1.12", "1.13", "1.14")
+        assert SUPPORTED_VERSIONS == ("1.9", "1.10", "1.11", "1.12", "1.13",
+                                      "1.14", "1.15")
         assert SUPPORTED_KINDS == (
             "Namespace", "CustomResourceDefinition", "ConfigMap", "Secret",
             "PersistentVolumeClaim", "ClusterRole", "ClusterRoleBinding", "Role",
