bcae764f9fefdb927d2bb065da6cbb112c102977
Oliver Nagy <olitheolix@gmail.com>
2020-04-22 15:59:52 +0200

    Add default API versions to the `k8sconfig` fixture.

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -16,6 +16,13 @@ def kind_available():
 
 def k8s_apis(config: K8sConfig):
     return {
+        ("ClusterRole", ""): K8sResource(
+            apiVersion="rbac.authorization.k8s.io/v1",
+            kind="ClusterRole",
+            name="clusterroles",
+            namespaced=False,
+            url=f"{config.url}/apis/rbac.authorization.k8s.io/v1",
+        ),
         ("ClusterRole", "rbac.authorization.k8s.io/v1"): K8sResource(
             apiVersion="rbac.authorization.k8s.io/v1",
             kind="ClusterRole",
@@ -29,6 +36,13 @@ def k8s_apis(config: K8sConfig):
             name="clusterroles",
             namespaced=False,
             url=f"{config.url}/apis/rbac.authorization.k8s.io/v1beta1",
+        ),
+        ("ClusterRoleBinding", ""): K8sResource(
+            apiVersion="rbac.authorization.k8s.io/v1",
+            kind="ClusterRoleBinding",
+            name="clusterrolebindings",
+            namespaced=False,
+            url=f"{config.url}/apis/rbac.authorization.k8s.io/v1",
         ),
         ("ClusterRoleBinding", "rbac.authorization.k8s.io/v1"): K8sResource(
             apiVersion="rbac.authorization.k8s.io/v1",
@@ -44,12 +58,26 @@ def k8s_apis(config: K8sConfig):
             namespaced=False,
             url=f"{config.url}/apis/rbac.authorization.k8s.io/v1beta1",
         ),
+        ("ConfigMap", ""): K8sResource(
+            apiVersion="v1",
+            kind="ConfigMap",
+            name="configmaps",
+            namespaced=True,
+            url=f"{config.url}/api/v1",
+        ),
         ("ConfigMap", "v1"): K8sResource(
             apiVersion="v1",
             kind="ConfigMap",
             name="configmaps",
             namespaced=True,
             url=f"{config.url}/api/v1",
+        ),
+        ("CronJob", ""): K8sResource(
+            apiVersion="batch/v1beta1",
+            kind="CronJob",
+            name="cronjobs",
+            namespaced=True,
+            url=f"{config.url}/apis/batch/v1beta1",
         ),
         ("CronJob", "batch/v1beta1"): K8sResource(
             apiVersion="batch/v1beta1",
@@ -58,12 +86,26 @@ def k8s_apis(config: K8sConfig):
             namespaced=True,
             url=f"{config.url}/apis/batch/v1beta1",
         ),
+        ("CustomResourceDefinition", ""): K8sResource(
+            apiVersion="apiextensions.k8s.io/v1beta1",
+            kind="CustomResourceDefinition",
+            name="customresourcedefinitions",
+            namespaced=False,
+            url=f"{config.url}/apis/apiextensions.k8s.io/v1beta1",
+        ),
         ("CustomResourceDefinition", "apiextensions.k8s.io/v1beta1"): K8sResource(
             apiVersion="apiextensions.k8s.io/v1beta1",
             kind="CustomResourceDefinition",
             name="customresourcedefinitions",
             namespaced=False,
             url=f"{config.url}/apis/apiextensions.k8s.io/v1beta1",
+        ),
+        ("DaemonSet", ""): K8sResource(
+            apiVersion="apps/v1",
+            kind="DaemonSet",
+            name="daemonsets",
+            namespaced=True,
+            url=f"{config.url}/apis/apps/v1",
         ),
         ("DaemonSet", "apps/v1"): K8sResource(
             apiVersion="apps/v1",
@@ -86,12 +128,26 @@ def k8s_apis(config: K8sConfig):
             namespaced=True,
             url=f"{config.url}/apis/extensions/v1beta1",
         ),
+        ("DemoCRD", ""): K8sResource(
+            apiVersion="mycrd.com/v1",
+            kind="DemoCRD",
+            name="democrds",
+            namespaced=True,
+            url=f"{config.url}/apis/mycrd.com/v1",
+        ),
         ("DemoCRD", "mycrd.com/v1"): K8sResource(
             apiVersion="mycrd.com/v1",
             kind="DemoCRD",
             name="democrds",
             namespaced=True,
             url=f"{config.url}/apis/mycrd.com/v1",
+        ),
+        ("Deployment", ""): K8sResource(
+            apiVersion="apps/v1",
+            kind="Deployment",
+            name="deployments",
+            namespaced=True,
+            url=f"{config.url}/apis/apps/v1",
         ),
         ("Deployment", "apps/v1"): K8sResource(
             apiVersion="apps/v1",
@@ -121,6 +177,13 @@ def k8s_apis(config: K8sConfig):
             namespaced=True,
             url=f"{config.url}/apis/extensions/v1beta1",
         ),
+        ("HorizontalPodAutoscaler", ""): K8sResource(
+            apiVersion="autoscaling/v1",
+            kind="HorizontalPodAutoscaler",
+            name="horizontalpodautoscalers",
+            namespaced=True,
+            url=f"{config.url}/apis/autoscaling/v1",
+        ),
         ("HorizontalPodAutoscaler", "autoscaling/v1"): K8sResource(
             apiVersion="autoscaling/v1",
             kind="HorizontalPodAutoscaler",
@@ -142,6 +205,13 @@ def k8s_apis(config: K8sConfig):
             namespaced=True,
             url=f"{config.url}/apis/autoscaling/v2beta2",
         ),
+        ("Ingress", ""): K8sResource(
+            apiVersion="networking.k8s.io/v1beta1",
+            kind="Ingress",
+            name="ingresses",
+            namespaced=True,
+            url=f"{config.url}/apis/networking.k8s.io/v1beta1",
+        ),
         ("Ingress", "extensions/v1beta1"): K8sResource(
             apiVersion="extensions/v1beta1",
             kind="Ingress",
@@ -156,6 +226,13 @@ def k8s_apis(config: K8sConfig):
             namespaced=True,
             url=f"{config.url}/apis/networking.k8s.io/v1beta1",
         ),
+        ("Job", ""): K8sResource(
+            apiVersion="batch/v1",
+            kind="Job",
+            name="jobs",
+            namespaced=True,
+            url=f"{config.url}/apis/batch/v1",
+        ),
         ("Job", "batch/v1"): K8sResource(
             apiVersion="batch/v1",
             kind="Job",
@@ -163,10 +240,24 @@ def k8s_apis(config: K8sConfig):
             namespaced=True,
             url=f"{config.url}/apis/batch/v1",
         ),
+        ("Namespace", ""): K8sResource(
+            apiVersion="v1",
+            kind="Namespace",
+            name="namespaces",
+            namespaced=False,
+            url=f"{config.url}/api/v1",
+        ),
         ("Namespace", "v1"): K8sResource(
             apiVersion="v1",
             kind="Namespace",
             name="namespaces",
+            namespaced=False,
+            url=f"{config.url}/api/v1",
+        ),
+        ("PersistentVolume", ""): K8sResource(
+            apiVersion="v1",
+            kind="PersistentVolume",
+            name="persistentvolumes",
             namespaced=False,
             url=f"{config.url}/api/v1",
         ),
@@ -177,10 +268,24 @@ def k8s_apis(config: K8sConfig):
             namespaced=False,
             url=f"{config.url}/api/v1",
         ),
+        ("PersistentVolumeClaim", ""): K8sResource(
+            apiVersion="v1",
+            kind="PersistentVolumeClaim",
+            name="persistentvolumeclaims",
+            namespaced=True,
+            url=f"{config.url}/api/v1",
+        ),
         ("PersistentVolumeClaim", "v1"): K8sResource(
             apiVersion="v1",
             kind="PersistentVolumeClaim",
             name="persistentvolumeclaims",
+            namespaced=True,
+            url=f"{config.url}/api/v1",
+        ),
+        ("Pod", ""): K8sResource(
+            apiVersion="v1",
+            kind="Pod",
+            name="pods",
             namespaced=True,
             url=f"{config.url}/api/v1",
         ),
@@ -189,7 +294,14 @@ def k8s_apis(config: K8sConfig):
             kind="Pod",
             name="pods",
             namespaced=True,
-            url=f"{config.url}/api/v1"
+            url=f"{config.url}/api/v1",
+        ),
+        ("PodDisruptionBudget", ""): K8sResource(
+            apiVersion="policy/v1beta1",
+            kind="PodDisruptionBudget",
+            name="poddisruptionbudgets",
+            namespaced=True,
+            url=f"{config.url}/apis/policy/v1beta1",
         ),
         ("PodDisruptionBudget", "policy/v1beta1"): K8sResource(
             apiVersion="policy/v1beta1",
@@ -203,21 +315,28 @@ def k8s_apis(config: K8sConfig):
             kind="Role",
             name="roles",
             namespaced=True,
-            url=f"{config.url}/apis/rbac.authorization.k8s.io/v1"
+            url=f"{config.url}/apis/rbac.authorization.k8s.io/v1",
         ),
         ("Role", "rbac.authorization.k8s.io/v1"): K8sResource(
             apiVersion="rbac.authorization.k8s.io/v1",
             kind="Role",
             name="roles",
             namespaced=True,
-            url=f"{config.url}/apis/rbac.authorization.k8s.io/v1"
+            url=f"{config.url}/apis/rbac.authorization.k8s.io/v1",
         ),
         ("Role", "rbac.authorization.k8s.io/v1beta1"): K8sResource(
             apiVersion="rbac.authorization.k8s.io/v1beta1",
             kind="Role",
             name="roles",
             namespaced=True,
-            url=f"{config.url}/apis/rbac.authorization.k8s.io/v1beta1"
+            url=f"{config.url}/apis/rbac.authorization.k8s.io/v1beta1",
+        ),
+        ("RoleBinding", ""): K8sResource(
+            apiVersion="rbac.authorization.k8s.io/v1",
+            kind="RoleBinding",
+            name="rolebindings",
+            namespaced=True,
+            url=f"{config.url}/apis/rbac.authorization.k8s.io/v1",
         ),
         ("RoleBinding", "rbac.authorization.k8s.io/v1"): K8sResource(
             apiVersion="rbac.authorization.k8s.io/v1",
@@ -233,10 +352,24 @@ def k8s_apis(config: K8sConfig):
             namespaced=True,
             url=f"{config.url}/apis/rbac.authorization.k8s.io/v1beta1",
         ),
+        ("Secret", ""): K8sResource(
+            apiVersion="v1",
+            kind="Secret",
+            name="secrets",
+            namespaced=True,
+            url=f"{config.url}/api/v1",
+        ),
         ("Secret", "v1"): K8sResource(
             apiVersion="v1",
             kind="Secret",
             name="secrets",
+            namespaced=True,
+            url=f"{config.url}/api/v1",
+        ),
+        ("Service", ""): K8sResource(
+            apiVersion="v1",
+            kind="Service",
+            name="services",
             namespaced=True,
             url=f"{config.url}/api/v1",
         ),
@@ -247,12 +380,26 @@ def k8s_apis(config: K8sConfig):
             namespaced=True,
             url=f"{config.url}/api/v1",
         ),
+        ("ServiceAccount", ""): K8sResource(
+            apiVersion="v1",
+            kind="ServiceAccount",
+            name="serviceaccounts",
+            namespaced=True,
+            url=f"{config.url}/api/v1",
+        ),
         ("ServiceAccount", "v1"): K8sResource(
             apiVersion="v1",
             kind="ServiceAccount",
             name="serviceaccounts",
             namespaced=True,
             url=f"{config.url}/api/v1",
+        ),
+        ("StatefulSet", ""): K8sResource(
+            apiVersion="apps/v1",
+            kind="StatefulSet",
+            name="statefulsets",
+            namespaced=True,
+            url=f"{config.url}/apis/apps/v1",
         ),
         ("StatefulSet", "apps/v1"): K8sResource(
             apiVersion="apps/v1",
@@ -282,13 +429,10 @@ def make_manifest(kind: str, namespace: str, name: str,
                   labels: Dict[str, str] = {}) -> dict:
     # Try to find the resource `kind` and lift its associated `apiVersion`.
     apis = k8s_apis(K8sConfig(version="1.15"))
-    candidates = [(_kind, _ver) for _kind, _ver in apis if _kind == kind]
-    if len(candidates) == 0:
+    try:
+        apiVersion = apis[(kind, "")].apiVersion
+    except KeyError:
         apiVersion = "v1"
-    else:
-        candidates.sort()
-        resource = apis[candidates.pop(0)]
-        apiVersion = resource.apiVersion
 
     # Compile a manifest.
     manifest: Dict[str, Any]
