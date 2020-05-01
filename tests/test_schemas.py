e8eb2bd0f91ff1cb32a5a1fff1ef5ab5b9c6ed31
Oliver Nagy <olitheolix@gmail.com>
2020-05-01 14:39:33 +0200

    Align the old schema definitions with the new filter structure.

--- tests/test_schemas.py
+++ tests/test_schemas.py
@@ -2,6 +2,44 @@ import square.schemas
 
 
 class TestMainGet:
-    def test_populate_schemas(self):
-        exclude = {"TEST": {"metadata": {"labels": "neither-bool-nor-dict"}}}
-        assert square.schemas.populate_schemas(exclude) is True
+    def test_sane_filter(self):
+        # Must be list.
+        assert square.schemas.valid([]) is True
+        assert square.schemas.valid({}) is False
+
+        # List must not contain empty strings.
+        assert square.schemas.valid([""]) is False
+        assert square.schemas.valid(["foo"]) is True
+
+        # Dictionaries must have exactly one key.
+        assert square.schemas.valid([{}]) is False
+        assert square.schemas.valid([{"foo": "foo"}]) is False
+        assert square.schemas.valid([{"foo": ["foo"]}]) is True
+        assert square.schemas.valid([{"foo": "foo", "bar": "bar"}]) is False
+
+        # List must only contain dictionaries and strings.
+        assert square.schemas.valid(["foo"]) is True
+        assert square.schemas.valid(["foo", {"bar": ["y"]}]) is True
+        assert square.schemas.valid(["foo", None]) is False
+
+        # Nested cases:
+        assert square.schemas.valid(["foo", {"bar": ["bar"]}]) is True
+        assert square.schemas.valid(["foo", {"bar": [{"x": ["x"]}]}]) is True
+        assert square.schemas.valid(["foo", {"bar": [{"x": "x"}]}]) is False
+
+    def test_default_filters(self):
+        assert square.schemas.default() == [
+            {"metadata": [
+                {"annotations": [
+                    "deployment.kubernetes.io/revision",
+                    "kubectl.kubernetes.io/last-applied-configuration",
+                    "kubernetes.io/change-cause",
+                ]},
+                "creationTimestamp",
+                "generation",
+                "resourceVersion",
+                "selfLink",
+                "uid",
+            ]},
+            "status",
+        ]
