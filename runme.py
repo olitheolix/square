c44a962d686d6cd408b712a8d3f36f2b2fe40264
Oliver Nagy <olitheolix@gmail.com>
2020-05-06 19:00:49 +0200

    Add Pyinstaller spec file for Square.

---
+++ runme.py
@@ -0,0 +1,11 @@
+"""Dummy entry point script for the Square package.
+
+Its primary purpose is to provide a single entrypoint script for PyInstaller.
+
+You may invoke Square from this folder with `python -m square` or `python runme.py`.
+
+"""
+import square
+import square.main
+
+square.main.main()
