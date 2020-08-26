10920d5eb5f5d90df22afb3aa5eeaf12a0e8f8e5
Oliver Nagy <olitheolix@gmail.com>
2020-08-26 15:35:33 +0200

    Intercept ctrl-c in interactive mode.

--- square/__main__.py
+++ square/__main__.py
@@ -3,4 +3,8 @@ import sys
 import square.main
 
 if __name__ == '__main__':
-    sys.exit(square.main.main())
+    try:
+        sys.exit(square.main.main())
+    except KeyboardInterrupt:
+        print("User abort")
+        sys.exit(1)
