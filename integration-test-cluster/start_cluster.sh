3e52ed1b42d12a5b42ca73d6092de9753e4a8905
Oliver Nagy <olitheolix@gmail.com>
2021-02-09 18:36:30 +0100

    Use Kind 1.18 for the integration tests (#292)
    * Use Kubernetes v1.18 for the integration tests.
    
    * Exclude "managedFields" by default.

--- integration-test-cluster/start_cluster.sh
+++ integration-test-cluster/start_cluster.sh
@@ -15,7 +15,7 @@ kind: Cluster
 apiVersion: kind.x-k8s.io/v1alpha4
 nodes:
 - role: control-plane
-  image: kindest/node:v1.16.4@sha256:b91a2c2317a000f3a783489dfb755064177dbc3a0b2f4147d50f04825d016f55
+  image: kindest/node:v1.18.15@sha256:5c1b980c4d0e0e8e7eb9f36f7df525d079a96169c8a8f20d8bd108c0d0889cc4
 EOF
 
 # Create cluster, then delete its config file.
