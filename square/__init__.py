f9cadcbe8ea8ae81593c5fa1618d840a8a00f418
Oliver Nagy <olitheolix@gmail.com>
2021-02-23 22:08:40 +0100

    Bump version: 1.1.3 → 1.1.4

--- square/__init__.py
+++ square/__init__.py
@@ -4,7 +4,7 @@ import sys
 from . import square
 from .cfgfile import load
 
-__version__ = '1.1.3'
+__version__ = '1.1.4'
 
 
 # ---------------------------------------------------------------------------
