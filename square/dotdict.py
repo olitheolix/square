06e53253171ed2d8f3fe61077b44d37e8cf81897
Oliver Nagy <olitheolix@gmail.com>
2023-07-15 14:09:54 +1000

    feat(typing): improve type annotations in `DotDict`.

--- square/dotdict.py
+++ square/dotdict.py
@@ -1,4 +1,5 @@
 import copy
+from typing import cast
 
 
 class DotDict(dict):
@@ -35,7 +36,7 @@ class DotDict(dict):
         return self.__deepcopy__(*args, **kwargs)
 
 
-def make(data):
+def make(data: dict):
     """Return `data` as a `DotDict`.
 
     This function will recursively replace all dictionary. It will also replace
@@ -44,28 +45,42 @@ def make(data):
     The intended input `data` of this function is any valid JSON structure.
 
     """
+    return cast(DotDict, _make(data))
+
+
+def _make(data):
+    """Recursively replace all Python dicts with `DotDict` instances."""
     if not isinstance(data, (list, tuple, dict)):
         return data
 
     # Recursively convert all elements in lists and dicts.
     if isinstance(data, (list, tuple)):
-        return [make(_) for _ in data]
+        return [_make(_) for _ in data]
     else:
-        return DotDict({k: make(v) for k, v in data.items()})
+        return DotDict({k: _make(v) for k, v in data.items()})
 
 
-def undo(data):
+def undo(data: DotDict | dict) -> dict:
     """Remove all `DotDict` instances from `data`.
 
     This function will recursively replace all `DotDict` instances with their
     plain Python equivalent.
 
     """
+    # Use an explicit cast because the `_undo` helper function is recursive and
+    # can thus, in theory, return something other than a dict. However, in
+    # practice it cannot because we already know that `data` is a dict and
+    # therefore so must be the return value.
+    return cast(dict, _undo(data))
+
+
+def _undo(data):
+    """Recursively replace all `DotDict` instances with their Python equivalent."""
     if not isinstance(data, (list, tuple, dict)):
         return data
 
     # Recursively convert all elements in lists and dicts.
     if isinstance(data, (list, tuple)):
-        return [undo(_) for _ in data]
+        return [_undo(_) for _ in data]
     else:
-        return dict({k: undo(v) for k, v in data.items()})
+        return dict({k: _undo(v) for k, v in data.items()})
