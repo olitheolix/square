86d3840c9a67fbc59614321bb03d5893a0b2784f
Oliver Nagy <olitheolix@gmail.com>
2023-07-21 23:31:47 +1000

    doc(examples): improve docu in examples/*

--- examples/as_library.py
+++ examples/as_library.py
@@ -1,6 +1,9 @@
 """Use Square as a library.
 
-Run it from the parent directory like so:
+This example leverages the Square library to import manifests from a server,
+create a plan and then apply it.
+
+Start the KinD cluster, then run this script from the parent directory like so:
 
   $ cd integration-test-cluster; ./start_cluster.sh; cd ..
   $ PYTHONPATH=`pwd` pipenv run python examples/as_library.py
@@ -23,7 +26,8 @@ def main():
     # Optional: Set log level (0 = ERROR, 1 = WARNING, 2 = INFO, 3 = DEBUG).
     square.square.setup_logging(3)
 
-    # Populate the `Config` structure. All main functions expect this.
+    # Populate the `Config` structure. All primary functions, ie `get`, `plan`
+    # and `apply` need one.
     config = Config(
         kubeconfig=kubeconfig,
         kubecontext=kubecontext,
@@ -31,11 +35,11 @@ def main():
         # Store manifests in this folder.
         folder=pathlib.PosixPath('manifests'),
 
-        # Organise the manifests in `folder` above in this order.
+        # Group the downloaded manifests by namespace, label and kind.
         groupby=GroupBy(label="app", order=["ns", "label", "kind"]),
 
-        # Specify the resources to operate on. These ones model the demo setup in
-        # the `../integration-test-cluster`.
+        # Specify the resources types to operate on. These ones in particular
+        # will work with the demo setup in the `../integration-test-cluster`.
         selectors=Selectors(
             kinds={"Deployment", "Service", "Namespace"},
             labels=["app=demoapp-1"],
@@ -50,14 +54,14 @@ def main():
     err = square.get(config)
     assert not err
 
-    # Compute the plan and show it. It will not show any differences because we
-    # planned against the resources we just downloaded.
+    # Create the plan and show it.
+    # NOTE: the plan will be clean since we only just downloaded the manifests.
     plan, err = square.plan(config)
     assert not err
     square.show_plan(plan)
 
-    # Apply the plan - will not actually do anything in this case, because the
-    # plan is empty.
+    # Apply the plan.
+    # NOTE: will do nothing because the plan was clean.
     err = square.apply_plan(config, plan)
     assert not err
 
