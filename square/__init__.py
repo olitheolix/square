d44300785c133f0d67e7328d25ae1a3173db92ed
Oliver Nagy <olitheolix@gmail.com>
2023-07-29 18:40:35 +1000

    Bump version: 1.4.0 → 1.5.0

--- square/__init__.py
+++ square/__init__.py
@@ -4,7 +4,7 @@ from pathlib import Path
 from . import square
 from .cfgfile import load
 
-__version__ = '1.4.0'
+__version__ = '1.5.0'
 
 
 # ---------------------------------------------------------------------------
