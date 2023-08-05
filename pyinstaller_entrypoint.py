e3f7995f013e1de6d8818e646d822ee9a69ff604
Oliver Nagy <olitheolix@gmail.com>
2023-08-05 20:18:20 +1000

    fix(pyinstaller): make binaries work with async framework.

--- pyinstaller_entrypoint.py
+++ pyinstaller_entrypoint.py
@@ -5,9 +5,7 @@ Its primary purpose is to provide an entrypoint for PyInstaller.
 You may invoke Square from this folder with `python -m square` or `python runme.py`.
 
 """
-import asyncio
-
 import square
 import square.main
 
-asyncio.run(square.main.main())
+square.main.main()
