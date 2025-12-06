6f33cd4ae2e37c381ef4bb719d5513dc02e6beda
Oliver Nagy <olitheolix@gmail.com>
2025-12-06 20:59:36 +1100

    Improve loading of default config resource.

--- square/__init__.py
+++ square/__init__.py
@@ -1,3 +1,4 @@
+import importlib.resources
 import sys
 from pathlib import Path
 
@@ -10,18 +11,19 @@ __version__ = '2.1.2'
 # ---------------------------------------------------------------------------
 # Global Runtime Constants
 # ---------------------------------------------------------------------------
-# Determine the base folder. This is usually the folder of this very file, but
+# Determine the base folder. This is usually the folder of this very file but
 # will be different if we run inside a bundle produced by PyInstaller.
 if getattr(sys, '_MEIPASS', None):
     BASE_DIR = Path(getattr(sys, '_MEIPASS'))
+    DEFAULT_CONFIG_FILE = BASE_DIR / "resources" / "defaultconfig.yaml"
 else:
-    BASE_DIR = Path(__file__).parent.parent
+    with importlib.resources.path("square.resources", "defaultconfig.yaml") as f:
+        DEFAULT_CONFIG_FILE = f
 
 # Square will source all its default values from this configuration file. The
 # only exceptions are the namespaces. By default, Square will target all the
-# namespaces whereas the specimen only declares "default" unless the user
-# explicitly says otherwise via the command line arguments or ".square.yaml".
-DEFAULT_CONFIG_FILE = BASE_DIR / "resources" / "defaultconfig.yaml"
+# namespaces whereas the specimen only declares the "default". The user
+# can override this via line arguments or a dedicated ".square.yaml".
 DEFAULT_CONFIG, err = load(DEFAULT_CONFIG_FILE)
 DEFAULT_CONFIG.selectors.namespaces.clear()
 assert not err
